47849562a08f59afbedfbd162506448c615274f1
Nikolay N. Nikolaev <esfimus@gmail.com>
2023-06-08 09:57:51 +0300

    View binding is added

--- app/src/main/java/com/esfimus/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/esfimus/popularlibraries/MainActivity.kt
@@ -2,10 +2,14 @@ package com.esfimus.popularlibraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.esfimus.popularlibraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ui: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ui = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 }
