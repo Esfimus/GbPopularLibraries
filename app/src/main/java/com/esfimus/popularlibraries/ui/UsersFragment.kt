d638081c14a3086a3eb51fbada1c9ba44ec8e676
Nikolay N. Nikolaev <esfimus@gmail.com>
2023-06-19 11:14:06 +0300

    Image could be converted and saved (.png)

--- app/src/main/java/com/esfimus/popularlibraries/ui/UsersFragment.kt
+++ app/src/main/java/com/esfimus/popularlibraries/ui/UsersFragment.kt
@@ -1,8 +1,12 @@
 package com.esfimus.popularlibraries.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
@@ -15,6 +19,8 @@ import com.esfimus.popularlibraries.mvp.view.UsersView
 import com.google.android.material.snackbar.Snackbar
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class UsersFragment : MvpAppCompatFragment(), UsersView, BackButtonListener {
 
@@ -34,6 +40,17 @@ class UsersFragment : MvpAppCompatFragment(), UsersView, BackButtonListener {
             }
         }
 
+    private val saveLauncher =
+        registerForActivityResult(ActivityResultContracts.CreateDocument("image/png")) { uri ->
+            try {
+                uri?.let { saveFile(it) }
+            } catch (e: Exception) {
+                snackMessage("Cannot save file")
+            }
+        }
+
+    private var bitmap: Bitmap? = null
+
     companion object { fun newInstance() = UsersFragment() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
@@ -52,14 +69,40 @@ class UsersFragment : MvpAppCompatFragment(), UsersView, BackButtonListener {
             adapter = RecyclerAdapter(presenter.usersListPresenter)
             recyclerUsers.adapter = adapter
 
-            convertButton.setOnClickListener {
+            openButton.setOnClickListener {
                 openLauncher.launch(arrayOf("image/jpeg"))
+            }
+
+            convertButton.setOnClickListener {
+                saveLauncher.launch("${timeStamp()}.png")
             }
         }
     }
 
+    @Suppress("DEPRECATION")
     private fun openFile(uri: Uri) {
         ui.image.setImageURI(uri)
+
+        try {
+            bitmap = if (Build.VERSION.SDK_INT >= 28) {
+                val source = ImageDecoder.createSource(requireActivity().contentResolver, uri)
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, uri)
+            }
+        } catch (e: Exception) {
+            snackMessage("Conversion problem")
+        }
+    }
+
+    private fun saveFile(uri: Uri) {
+        requireActivity().contentResolver.openOutputStream(uri)?.use {
+            bitmap?.compress(Bitmap.CompressFormat.PNG, 100, it)
+        } ?: throw IllegalStateException("Cannot open output stream")
+    }
+
+    private fun timeStamp(): String {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm-ss"))
     }
 
     @SuppressLint("NotifyDataSetChanged")
